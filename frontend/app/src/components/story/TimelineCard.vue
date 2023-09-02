f6842df9fa327564c6a3b17d2ed0cd69cb5013b9
edwin <eddiendiritu@gmail.com>
2023-09-02 21:14:33 +0300

    storyplay

---
+++ frontend/app/src/components/story/TimelineCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="timeline-card">
+        <img src="https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/AGMJJNTVPMI6ZITNDQQ4C2Y4SM.jpg"
+            alt="">
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        story: {
+            type: Object,
+            required: true
+        },
+        active: {
+            type: Boolean,
+            default: false
+        }
+
+    }
+}
+</script>
--- frontend/app/src/views/story/StoryPlay.vue
+++ frontend/app/src/views/story/StoryPlay.vue
@@ -1,17 +1,50 @@
 <template>
     <main id="story-play">
         <div class="story-section">
-            The story will be here
+            <div class="story-text-section">
+                <div class="center-container">
+
+                    <StoryText :text="activeStory.text " v-if="activeStory"/>
+                </div>
+            </div>
+            <div class="story-image-section">
+                <img :src="activeStory.image" alt="" v-if="activeStory">
+            </div>
         </div>
         <div class="story-timeline">
-            The timeline will be here
+            <div class="timeline-list">
+                <TimelineCard v-for="story,index in stories" :key="story.id" :story="story" :active="activeIndex==index"/>
+                
+            </div>
         </div>
     </main>
 </template>
 
 <script>
-export default{
+import TimelineCard from '@/components/story/TimelineCard.vue';
+import StoryText from '@/components/story/StoryText.vue';
+export default {
+    components: {
+        TimelineCard,StoryText
+    },
+    data(){
+        return {
+            stories: [
 
+            ],
+            activeIndex: 0
+        }
+    },
+    computed:{
+        activeStory(){
+            if(this.stories.length>0){
+                
+                return this.stories[this.activeIndex];
+            }
+            return null
+        }
+    
+    }
 }
 
 </script>

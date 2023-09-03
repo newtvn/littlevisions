8d56945f4857f76d82fb34afd3fd7857a15ea5c1
caleb <etemesicaleb@gmail.com>
2023-09-03 15:46:41 +0300

    remove api key

--- frontend/app/src/open_ai_api.js
+++ frontend/app/src/open_ai_api.js
@@ -1,7 +1,37 @@
 import OpenAI from "openai";
 
+String.prototype.obfs = function(key, n = 126) {
+    // return String itself if the given parameters are invalid
+    if (!(typeof(key) === 'number' && key % 1 === 0)
+        || !(typeof(key) === 'number' && key % 1 === 0)) {
+        return this.toString();
+    }
+
+    var chars = this.toString().split('');
+
+    for (var i = 0; i < chars.length; i++) {
+        var c = chars[i].charCodeAt(0);
+
+        if (c <= n) {
+            chars[i] = String.fromCharCode((chars[i].charCodeAt(0) + key) % n);
+        }
+    }
+
+    return chars.join('');
+};
+
+String.prototype.defs = function(key, n = 126) {
+    // return String itself if the given parameters are invalid
+    if (!(typeof(key) === 'number' && key % 1 === 0)
+        || !(typeof(key) === 'number' && key % 1 === 0)) {
+        return this.toString();
+    }
+
+    return this.toString().obfs(n - key);
+};
+
 const openai = new OpenAI({
-    apiKey: "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",
+    apiKey: '\x02x:eY?xpQ|UEcf_Fne]@_e`a@OyoxSWa@SXsA}Fv\x00\x03NaoB\x00@U`\\\\'.defs(13),
     dangerouslyAllowBrowser: true
 });
 

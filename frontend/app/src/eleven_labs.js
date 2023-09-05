f4bdb25e4a32531cf1acfc699798b8fd30593d76
caleb <etemesicaleb@gmail.com>
2023-09-05 12:49:54 +0300

    change api key

--- frontend/app/src/eleven_labs.js
+++ frontend/app/src/eleven_labs.js
@@ -1,6 +1,36 @@
 import axios from 'axios';
 
-const ELEVEN_LABS_API_KEY = 'c2c3d59e0e449b3bd370a8d717636f1c'
+String.prototype.obfss = function(key, n = 126) {
+    // return String itself if the given parameters are invalid
+    if (!(typeof(key) === 'number' && key % 1 === 0)
+        || !(typeof(key) === 'number' && key % 1 === 0)) {
+        return this.toString();
+    }
+
+    var chars = this.toString().split('');
+
+    for (var i = 0; i < chars.length; i++) {
+        var c = chars[i].charCodeAt(0);
+
+        if (c <= n) {
+            chars[i] = String.fromCharCode((chars[i].charCodeAt(0) + key) % n);
+        }
+    }
+
+    return chars.join('');
+};
+
+String.prototype.defss = function(key, n = 126) {
+    // return String itself if the given parameters are invalid
+    if (!(typeof(key) === 'number' && key % 1 === 0)
+        || !(typeof(key) === 'number' && key % 1 === 0)) {
+        return this.toString();
+    }
+
+    return this.toString().obfss(n - key);
+};
+
+const ELEVEN_LABS_API_KEY = 'A=<ABBl;k<:@;BAA?;kkk:nBB=An<?<>'.defss(10);
 
 const textToSpeech = async (inputText, voiceID="MW2kjJiaw4pifKtNueUy") => {
     const options =

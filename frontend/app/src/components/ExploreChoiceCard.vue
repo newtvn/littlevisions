2ff65b2073d4044884e06a9ff44fc417135c26c4
edwin <eddiendiritu@gmail.com>
2023-09-20 16:25:56 +0300

    explore choices complete

---
+++ frontend/app/src/components/ExploreChoiceCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="explore-choice-card" :class="{ selected: selected }">
+        <div class="selected-bar-indicator">
+
+        </div>
+        <p class="explore-choice-card-title">
+            {{ choice.title }}
+        </p>
+
+        <div class="explore-choice-card-mood-container">
+            <p class="explore-choice-card-mood">
+                {{ moodEmoji }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        choice: {
+            type: Object,
+            required: true
+        },
+        selected: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        moodEmoji() {
+            var mood = this.$props.choice.mood;
+            if (mood === 'sad') {
+                return "😭"
+            }
+            else if (mood === 'scary') {
+                return "😨"
+            }
+            else if (mood === 'happy') {
+                return "😃"
+            }
+            else if (mood === 'funny') {
+                return "😂"
+            }
+
+            return "🙂"
+        }
+    }
+
+}
+</script>
--- frontend/app/src/components/panels/ExploreChoicesPanel.vue
+++ frontend/app/src/components/panels/ExploreChoicesPanel.vue
@@ -1,3 +1,42 @@
 <template>
-    Explore Choices Panel Here
+    <div class="header-container">
+        <AvatarSpeak :text="avatar_text" type="secondary" />
+    </div>
+    <div class="slideout-body story-content">
+        <div class="flex-wrap explore-choice-card-list">
+            <ExploreChoiceCard v-for="choice,index in choices" :key="index" :choice="choice" :selected="choice==selectedChoice" @click="selectedChoice=choice"/>
+        </div>
+
+    </div>
+    <div class="slideout-footer footer-container ">
+
+
+        <button class="primary-btn full-width bg-secondary">
+            Start Writing
+        </button>
+    </div>
 </template>
+
+<script>
+import AvatarSpeak from '../AvatarSpeak.vue';
+import ExploreChoiceCard from '../ExploreChoiceCard.vue';
+export default {
+    components: {
+        AvatarSpeak,ExploreChoiceCard
+    },
+    data() {
+        return {
+            avatar_text: "Hello! Choose what your story will be about. Click on your favorite option below to start your adventure!",
+            choices: [
+                { "title": "The Big African Boy", "mood": "sad" },
+                { "title": "The Beautiful Princess", "mood": "happy" },
+                { "title": "The Forgetful Dragon", "mood": "funny" },
+                { "title": "The Haunted House", "mood": "scary" },
+                { "title": "The Selfish Rat", "mood": "funny" },
+                { "title": "The Mysterious Cat", "mood": "" },
+            ],
+            selectedChoice: null
+        }
+    }
+}
+</script>

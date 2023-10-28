94e6d227e60a561e6a8830a84d8f76880ec9e083
eddie <eddiendiritu@gmail.com>
2023-10-28 16:56:39 +0300

    nginx basic

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,6 +1,6 @@
-# import os
-# from .openai_key import openai_key,stabilityai_key,eleven_labs_key,REPLICATE_API_TOKEN
-# os.environ['OPENAI_API_KEY'] = openai_key
-# os.environ['STABILITYAI_API_KEY'] = stabilityai_key
-# os.environ['ELEVEN_LABS_API_KEY'] = eleven_labs_key
-# os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
+import os
+from .openai_key import openai_key,stabilityai_key,eleven_labs_key,REPLICATE_API_TOKEN
+os.environ['OPENAI_API_KEY'] = openai_key
+os.environ['STABILITYAI_API_KEY'] = stabilityai_key
+os.environ['ELEVEN_LABS_API_KEY'] = eleven_labs_key
+os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
